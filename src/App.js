28779a92cbb1133c0085416640144b3b00a4dffd
Jieun Lee <najee0610@naver.com>
2022-05-11 12:53:54 +0900

    js&css 수정, fade추가, 사진추가, 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-
-import Introduce from './Introduce';
+import Introduce from "./Introduce";
 
 function App() {
-  return(
-    <Introduce name="이지은"></Introduce>
-    
-  );
-
+  return <Introduce name="이지은" age='22' major="융합소프트웨어학부 데이터테크놀로지학과" ></Introduce>;
 }
 
 export default App;
--- src/Introduce.js
+++ src/Introduce.js
@@ -1,84 +1,136 @@
 import { useState } from "react";
 import React from "react";
-import './Introduce.css';
-import me from './images/me.jpg'
-import insta from './images/insta.jpg'
-import myongji from './images/myongji.png'
-import puppy from './images/puppy.jpg'
+import "./Introduce.css";
+import me from "./images/me.jpg";
+import insta from "./images/insta.jpg";
+import myongji from "./images/myongji.png";
+import puppy from "./images/puppy.jpg";
+import nolla from "./images/nolla.jpg";
+import spam from "./images/instaSpam.jpg";
+import Fade from "react-reveal/Fade"; // Import react-reveal(Fade)
+
+
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+const colorW = "white";
+const colorO = "orange";
+
 
 function Introduce(props) {
-    
-    const [name, setName] = useState("");
-    const [age, setAge] = useState(0);
-    const [instaId, setInstaId] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [instaId, setInstaId] = useState("");
 
+  function onChangeName(e) {
+    setName(e.target.value);
+  }
 
-    function onChangeName(e){
-        setName(e.target.value);
-        
-    };
+  function onChangeAge(e) {
+    setAge(e.target.value);
+  }
 
-    function onChangeAge(e){
-        setAge(e.target.value);
-    }
+  function onChangeInstaId(e) {
+    setInstaId(e.target.value);
+  }
 
-    function onChangeInstaId(e){
-        setInstaId(e.target.value);
-    }
+  return (
+    <div>
+      <div className="App">
+        <div
+          css={css`
+            padding: 32px;
+            background-color: #ffa300;
+            font-size: 32px;
+            border-radius: 4px;
+            &:hover {
+              color: ${colorW};
+            }
+          `}
+        >
+          멋쟁이사자처럼 웹 아기사자 <br />
+          이지은을 소개합니다 :-D{" "}
+        </div>
+        <Fade bottom>
+        <div class="introduceBox">
+          안녕하세요! 저는 <br />
+          <img src={me} width="200px"></img>
+          <br /> {props.name} <br />
+          입니다.
+        </div>
+        <div class="introduceBox">
+          제 나이는 {props.age}살 20학번이구요,
+          <br />
+          <img src={myongji} width="500px"></img>
+          <br />
+          {props.major}예요.
+          <br />
+        </div>
+        <div class="introduceBox">
+          <img src={insta} width="250px"></img> <br />
+          저의 인스타그램 아이디는 <br />
+          @i_ji_silver
+          <br />
+          입니다 :-){" "}
+        </div>{" "}
+        <hr />
+        </Fade>
 
-    function imgs(){
-        alert("hello");
-    }
-
-   
-
-
-   
-    return(
-        <div> 
-            <h3>멋쟁이사자처럼 웹 아기사자 <br/>
-                이지은을 소개합니다 :-D
-            </h3>
-            <p>안녕하세요! 저는 <br/>
-            <img src={me} width='200px'></img>
-
-            <br/>  {props.name} <br/>입니다.</p>
-            <p>제 나이는 22살 20학번이구요,<br/>
-            <img src={myongji} width='500px'></img><br/>
-                융합소프트웨어학부 데이터테크놀로지학과예요.
-
-            </p> 
-                
-            <img src={insta} width='250px'></img>
-            <p>저의 인스타그램 아이디는 <br/>
-            i_ji_silver<br/>
-            입니다 :-)  </p> <hr/>
-
-                
-
-            <div>
-                여러분들도 소개해주세요.<br/>
-                이름 : <input onChange={onChangeName} value={name}/><button>확인</button>
-            <p>{name}님 안녕하세요 :) </p>
+        <Fade bottom>
+        <div>
+          <div id="your_introduce">
+            <div
+              class="introduceBox"
+              css={css`
+                padding: 32px;
+                margin-bottom: 70px;
+                background-color: #6db500;
+                font-size: 32px;
+                border-radius: 4px;
+                &:hover {
+                  color: ${colorO};
+                }
+              `}
+            >
+              여러분들도 소개해주세요.
+              <br />
             </div>
-
+            이름 : <input onChange={onChangeName} value={name} />
+            <button onClick={onChangeName}>확인</button>
             <div>
-                나이 : <input onChange={onChangeAge} value={age}/><button>확인</button>
-                
-                <p>{age}살 이시군요.</p>
-
+              {name}님 안녕하세요 :)
+              <br />
+              <br />
+              <img src={puppy} width="400px"></img>{" "}
             </div>
+          </div>
 
+          <div class="introduceBox">
+            나이 : <input onChange={onChangeAge} value={age} />
+            <button>확인</button>
             <div>
-                인스타그램 : <input onChange={onChangeInstaId} value={instaId}/><button>확인</button>
-                <p>@{instaId} 반가워요. 저랑 맞팔로우해요~</p> 
-
+              {age}살 이시군요.
+              <br />
+              <br />
+              <img src={nolla} width="250px"></img> <br />
             </div>
+          </div>
 
-
-        </div> 
-    )
-
-  
+          <div class="introduceBox">
+            인스타그램 : <input onChange={onChangeInstaId} value={instaId} />
+            <button>확인</button>
+            <div>
+              @{instaId} 반가워요. 저랑 맞팔로우해요~
+              <br />
+              스팸아님
+              <br />
+              <img src={spam} width="250px"></img> <br />
+            </div>
+          </div>
+        </div>
+        </Fade>
+      </div>
+    </div>
+  );
 }
+
 export default Introduce;
