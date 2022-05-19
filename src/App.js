5c0742ba1a9ecaa25df3d1311846e7522b50f3df
Jieun Lee <najee0610@naver.com>
2022-05-19 18:33:55 +0900

    week5 commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import InputBox from "./Components/InputBox";
 import Home from './Home';
-
+//
 function App() {
   return <Home/>;
- 
+  
 }
 
 export default App;
